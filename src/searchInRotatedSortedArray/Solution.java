6bb0fceb6a81b8e478d9ee8135447eb1bf2d54fe
Arrow Cen <arrow.cen@gmail.com>
2014-01-15 16:27:04 -0500

    non recursive (non hassle) solution

--- src/searchInRotatedSortedArray/Solution.java
+++ src/searchInRotatedSortedArray/Solution.java
@@ -1,7 +1,39 @@
 package searchInRotatedSortedArray;
 
 public class Solution {
+
 	public int search(int[] A, int target) {
+		// don't recurse when you can use while!
+		int l = 0, r = A.length - 1;
+
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (A[m] == target)
+				return m;
+			// left half sorted
+			// when A[l] == A[m], we have reach left bound, need to go right
+			if (A[l] <= A[m]) {
+				// we can definitely go left, go left
+				if (A[l] <= target && target < A[m]) {
+					r = m - 1;
+				} else {
+					l = m + 1;
+				}
+			}
+			// right half sorted
+			else {
+				// we can definitely go right, go right
+				if (A[m] < target && target <= A[r]) {
+					l = m + 1;
+				} else {
+					r = m - 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int searchRec(int[] A, int target) {
 		return doSearch(A, 0, A.length - 1, target);
 	}
 
@@ -19,13 +51,13 @@ public class Solution {
 			// left half must be smaller than target, need to go right
 			if (A[mid] >= A[start]) {
 				return doSearch(A, mid + 1, end, target);
-			} 
+			}
 			// break point is at left half, need to consider two scenarios
 			else {
 				// A[mid] < A[start] <= target, go left
 				if (target >= A[start]) {
 					return doSearch(A, start, mid - 1, target);
-				} 
+				}
 				// A[mid] < target < A[start], go right
 				else {
 					return doSearch(A, mid + 1, end, target);
@@ -39,7 +71,7 @@ public class Solution {
 				// target <= A[end] < A[mid], go right
 				if (target <= A[end]) {
 					return doSearch(A, mid + 1, end, target);
-				} 
+				}
 				// A[end] < target < A[mid], go left
 				else {
 					return doSearch(A, start, mid - 1, target);
@@ -49,7 +81,7 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		int[] A = { 4, 5, 6, 7, 8, 1, 2, 3 };
+		int[] A = { 3, 1 };
 		// int target = 3;
 		for (int target : A)
 			System.out.println(new Solution().search(A, target));
