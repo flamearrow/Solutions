202a54a9bf14c436e757f955e6a90a4e2e33f049
Arrow Cen <arrow.cen@gmail.com>
2014-01-15 10:16:21 -0500

    update search in rotated array

--- src/searchInRotatedSortedArray/Solution.java
+++ src/searchInRotatedSortedArray/Solution.java
@@ -1,12 +1,5 @@
 package searchInRotatedSortedArray;
 
-//Suppose a sorted array is rotated at some pivot unknown to you beforehand.
-//
-//(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-//
-//You are given a target value to search. If found in the array return its index, otherwise return -1.
-//
-//You may assume no duplicate exists in the array.
 public class Solution {
 	public int search(int[] A, int target) {
 		return doSearch(A, 0, A.length - 1, target);
@@ -22,24 +15,43 @@ public class Solution {
 		int mid = (start + end) / 2;
 		if (A[mid] == target)
 			return mid;
-		if (A[mid] < target) {
-			if (target > A[end]) {
-				return doSearch(A, start, mid - 1, target);
-			} else {
+		if (target > A[mid]) {
+			// left half must be smaller than target, need to go right
+			if (A[mid] >= A[start]) {
 				return doSearch(A, mid + 1, end, target);
+			} 
+			// break point is at left half, need to consider two scenarios
+			else {
+				// A[mid] < A[start] <= target, go left
+				if (target >= A[start]) {
+					return doSearch(A, start, mid - 1, target);
+				} 
+				// A[mid] < target < A[start], go right
+				else {
+					return doSearch(A, mid + 1, end, target);
+				}
 			}
 		} else {
-			if (A[start] > target) {
-				return doSearch(A, mid + 1, end, target);
-			} else {
+			// right half must be bigger than target, go left
+			if (A[mid] <= A[end]) {
 				return doSearch(A, start, mid - 1, target);
+			} else {
+				// target <= A[end] < A[mid], go right
+				if (target <= A[end]) {
+					return doSearch(A, mid + 1, end, target);
+				} 
+				// A[end] < target < A[mid], go left
+				else {
+					return doSearch(A, start, mid - 1, target);
+				}
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] A = { 1, 2, 3, 4, 5 };
-		int target = 6;
-		System.out.println(new Solution().search(A, target));
+		int[] A = { 4, 5, 6, 7, 8, 1, 2, 3 };
+		// int target = 3;
+		for (int target : A)
+			System.out.println(new Solution().search(A, target));
 	}
 }
