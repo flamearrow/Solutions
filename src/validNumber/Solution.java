ffca4218c2e8dcd2b72c5ca21bbcac14b1f50770
Arrow Cen <arrow.cen@gmail.com>
2014-01-17 14:36:28 -0500

    valid number
    the trick is when entering a new state, we need to make sure what is the previous state so that we know what the next state would be

---
+++ src/validNumber/Solution.java
@@ -0,0 +1,126 @@
+package validNumber;
+
+//Validate if a given string is numeric.
+//
+//Some examples:
+//"0" => true
+//" 0.1 " => true
+//"abc" => false
+//"1 a" => false
+//"2e10" => true
+//
+//Note: It is intended for the problem statement to be ambiguous. 
+//You should gather all requirements up front before implementing one. 
+
+// some edge cases
+// "2 e10" is it valid? -false
+// "2e 10" is it valid? -false
+// "2E10" is it valid? -true
+// "20E9" is it valid? -true
+// "2E01" is it valid? -true
+// ".1" is it valid? -true
+// "3." is it valid? -ture
+// "." -false
+// "46.e3"
+public class Solution {
+	// state transfer, what's valid for next char?
+	// digit - state1
+	// space - state2
+	// e/E - state3
+	// . - state4
+	// plusminus
+
+	// the trick is when entering a new state, we need to make sure what is the
+	// previous state so that we know what the next state would be
+
+	public static int DIGIT = 1 << 0;
+	public static int SPACE = 1 << 1;
+	public static int eE = 1 << 2;
+	public static int DOT = 1 << 3;
+	public static int PLUSMINUS = 1 << 4;
+	public static int LEADING_SPACE = DIGIT | DOT | SPACE | PLUSMINUS;
+	public static int LEADING_PLUSMINUS = DIGIT | DOT;
+	public static int TRAILING_SPACE = SPACE;
+
+	public boolean isNumber(String s) {
+		if (s.length() == 0)
+			return false;
+		int cur = 0;
+		int state = LEADING_SPACE;
+		while (cur < s.length()) {
+			char c = s.charAt(cur);
+			cur++;
+			// space is legal
+			if (c == ' ' && ((state & SPACE) > 0)) {
+				// can only be in leading or trailing
+				if (state != LEADING_SPACE) {
+					state = TRAILING_SPACE;
+				} else {
+					// we have found an empty string
+					if (cur == s.length())
+						return false;
+				}
+			}
+			// digit is legal
+			else if (isDigit(c) && ((state & DIGIT) > 0)) {
+				// we find first digit
+				if (state == LEADING_SPACE || state == LEADING_PLUSMINUS) {
+					state = DIGIT | DOT | eE | SPACE;
+				}
+				// we have seen eE before
+				else if (state == (DIGIT | PLUSMINUS)) {
+					state = DIGIT | SPACE;
+				}
+				// we have seen dot before
+				else if (state == DIGIT || state == (DIGIT | SPACE | eE)) {
+					state = DIGIT | SPACE | eE;
+				}
+
+				// otherwise are still expecting DIGIT | DOT | eE | SPACE
+			}
+			// dot is legal
+			else if (c == '.' && ((state & DOT) > 0)) {
+				// if we have found nothing before the dot, we can only expect
+				// for a digit, because "." won't be valid
+				if (state == LEADING_SPACE || state == LEADING_PLUSMINUS) {
+					// if we have reached end then we have found something like
+					// "  +." or "." or " -.", which isn't valid, return false
+					if (cur == s.length())
+						return false;
+					state = DIGIT;
+				}
+				// otherwise we expect digit or space or eE, "2.e10" is valid
+				else
+					state = DIGIT | SPACE | eE;
+			}
+			// eE is legal
+			else if ((c == 'e' || c == 'E') && ((state & eE) > 0)) {
+				// we have found something ends with eE
+				if (cur == s.length()) {
+					return false;
+				}
+				// after eE, we expect digit or plusminus
+				state = DIGIT | PLUSMINUS;
+			} else if ((c == '-' || c == '+') && (state & PLUSMINUS) > 0) {
+				// we have found something end with -
+				if (cur == s.length())
+					return false;
+				// note "1.3e-2.2" is valid, after e we can still have dot
+				state = LEADING_PLUSMINUS;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	boolean isDigit(char c) {
+		return ('0' <= c) && (c <= '9');
+	}
+
+	public static void main(String[] args) {
+		String[] ss = { "1.3e-2.2 ", "2.e10", "3.1.3" };
+		for (String s : ss)
+			System.out.println(new Solution().isNumber(s));
+	}
+}
