7da5e98b5b4f7c7cfe71833175ad80ee8caad78e
Chen <Chen@flamearrow-pc.home>
2014-02-05 22:57:22 -0500

    human readable code for singleNumber2

--- src/singleNumber2/Solution.java
+++ src/singleNumber2/Solution.java
@@ -28,26 +28,26 @@ public class Solution {
 		int oneToTwo;
 		int twoToThree;
 		int threeToOne;
- 		for (int i : A) {
- 			// how to shift the bit:
- 			//  first we need to know which bit to be moved to next level
- 			//   i.e 
- 			// 		ones = 011, i = 001, then the last bit needs to be moved, so oneToTwo=ones&i = 001
- 			//  after calculating all bits to be moved to next level, 
- 			//   we cleared the bit moved from here to next, and add the bit moved from previous to here
- 			//   i.e
- 			// 		ones = 011, i = 001, threeToOne = 100
- 			// 		 then we first clear the last bit of ones, since it's moved to twos
- 			// 				ones = 010
- 			// 		 then we move the bit from three
- 			// 				ones = 110
- 			//
- 			//  note in the case where the ith bit are all 0, and i contains 1 for that bit, 
- 			//   the previous approach won't work
- 			//   i.e 
- 			//  	ones = 000, twos = 000, threes = 000, i = 111
- 			//   in this case we need to add the additional bit to ones
- 			//   so we have ones |= (^(ones|twos|threes)) & i
+		for (int i : A) {
+			// how to shift the bit:
+			//  first we need to know which bit to be moved to next level
+			//   i.e 
+			// 		ones = 011, i = 001, then the last bit needs to be moved, so oneToTwo=ones&i = 001
+			//  after calculating all bits to be moved to next level, 
+			//   we cleared the bit moved from here to next, and add the bit moved from previous to here
+			//   i.e
+			// 		ones = 011, i = 001, threeToOne = 100
+			// 		 then we first clear the last bit of ones, since it's moved to twos
+			// 				ones = 010
+			// 		 then we move the bit from three
+			// 				ones = 110
+			//
+			//  note in the case where the ith bit are all 0, and i contains 1 for that bit, 
+			//   the previous approach won't work
+			//   i.e 
+			//  	ones = 000, twos = 000, threes = 000, i = 111
+			//   in this case we need to add the additional bit to ones
+			//   so we have ones |= (^(ones|twos|threes)) & i
 			oneToTwo = i & ones;
 			twoToThree = i & twos;
 			threeToOne = i & threes;
@@ -59,8 +59,28 @@ public class Solution {
 		return ones;
 	}
 
+	// this one is easer to understand but have two iterations and uses a bigger buffer
+	public int singleNumber2(int[] A) {
+		int[] back = new int[32];
+		for (int i : A) {
+			for (int shift = 0; shift < 32; shift++) {
+				if (((i >> shift) & 1) > 0) {
+					back[shift] = (back[shift] + 1) % 3;
+				}
+			}
+		}
+
+		int ret = 0;
+		for (int i = 0; i < 32; i++) {
+			if (back[i] == 1) {
+				ret |= (1 << i);
+			}
+		}
+		return ret;
+	}
+
 	public static void main(String[] args) {
 		int[] A = { 1, 1, 1, 2, 3, 3, 3 };
-		System.out.println(new Solution().singleNumber(A));
+		System.out.println(new Solution().singleNumber2(A));
 	}
 }
