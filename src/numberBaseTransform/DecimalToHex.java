03324387a44b832b59480d1b191788d8a747c8f7
Chen <Chen@flamearrow-pc.home>
2014-02-10 23:20:25 -0500

    decimalToHex

---
+++ src/numberBaseTransform/DecimalToHex.java
@@ -0,0 +1,20 @@
+package numberBaseTransform;
+
+public class DecimalToHex {
+
+	static String decimalToHex(int decimal) {
+		StringBuilder sb = new StringBuilder();
+		// use a table to do fast conversion...
+		String base16 = "0123456789ABCDEF";
+		while (decimal > 0) {
+			sb.insert(0, base16.charAt(decimal % 16));
+			decimal /= 16;
+		}
+		sb.insert(0, "0x");
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(decimalToHex(123456));
+	}
+}
