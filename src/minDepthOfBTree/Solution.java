365851f396457b203d79ebbfc24581bb2adffa8b
Chen <Chen@flamearrow-pc.home>
2014-02-13 23:39:28 -0500

    min depth of bTree in BFS

--- src/minDepthOfBTree/Solution.java
+++ src/minDepthOfBTree/Solution.java
@@ -1,9 +1,49 @@
 package minDepthOfBTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //Given a binary tree, find its minimum depth.
 //
 //The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 public class Solution {
+	// this is faster
+	public int minDepthBFS(TreeNode root) {
+		if (root == null)
+			return 0;
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		TreeNode separator = new TreeNode(0);
+		q.offer(root);
+		q.offer(separator);
+		int lvl = 1;
+		boolean lvlEnd = false;
+		while (!q.isEmpty()) {
+			TreeNode next = q.poll();
+			if (next == separator) {
+				break;
+			}
+			if (q.peek() == separator) {
+				lvlEnd = true;
+				q.poll();
+			}
+			if (next.left == null && next.right == null) {
+				return lvl;
+			}
+			if (next.left != null) {
+				q.offer(next.left);
+			}
+			if (next.right != null) {
+				q.offer(next.right);
+			}
+			if (lvlEnd) {
+				q.offer(separator);
+				lvlEnd = false;
+				lvl++;
+			}
+		}
+		return lvl;
+	}
+
 	public int minDepth(TreeNode root) {
 		if (root == null)
 			return 0;
