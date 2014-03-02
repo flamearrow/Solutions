4d8a5c55956b54c2da2a1e4df59f5067dbb1ed4e
flamearrow <arrow.cen@gmail.com>
2014-03-01 19:24:44 -0500

    add a new way to do level order traversal of a tree

--- src/bTreeLevelOrderTraversal/Solution.java
+++ src/bTreeLevelOrderTraversal/Solution.java
@@ -2,6 +2,7 @@ package bTreeLevelOrderTraversal;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 
 //Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
@@ -20,6 +21,33 @@ import java.util.Queue;
 //  [15,7]
 //]
 public class Solution {
+	// another way is to use a count to count the remaining node in current level
+	//  when we are done with current level, set count to the size of the queue 
+	//  because now queue contains all nodes from next level
+	public ArrayList<ArrayList<Integer>> levelOrder2(TreeNode root) {
+		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+		if (root == null)
+			return ret;
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		q.offer(root);
+		int count = q.size();
+		ArrayList<Integer> curLevel = new ArrayList<Integer>();
+		while (!q.isEmpty()) {
+			TreeNode next = q.poll();
+			curLevel.add(next.val);
+			if (next.left != null)
+				q.offer(next.left);
+			if (next.right != null)
+				q.offer(next.right);
+			if (--count == 0) {
+				ret.add(curLevel);
+				curLevel = new ArrayList<Integer>();
+				count = q.size();
+			}
+		}
+		return ret;
+	}
+
 	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
 		// BFS
 		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
@@ -60,12 +88,12 @@ public class Solution {
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(3);
-		// root.left = new TreeNode(9);
-		// root.right = new TreeNode(20);
-		// root.right.left = new TreeNode(15);
-		// root.right.right = new TreeNode(7);
+		root.left = new TreeNode(9);
+		root.right = new TreeNode(20);
+		root.right.left = new TreeNode(15);
+		root.right.right = new TreeNode(7);
 
-		ArrayList<ArrayList<Integer>> ret = new Solution().levelOrder(root);
+		ArrayList<ArrayList<Integer>> ret = new Solution().levelOrder2(root);
 		System.out.println(ret);
 	}
 }
