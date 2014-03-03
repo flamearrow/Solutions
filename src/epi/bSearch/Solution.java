02b0ceffdd672962a4aa7bb219508134aba46dfb
flamearrow <arrow.cen@gmail.com>
2014-03-02 22:17:51 -0500

    bsearch instert

--- src/epi/bSearch/Solution.java
+++ src/epi/bSearch/Solution.java
@@ -22,10 +22,32 @@ public class Solution {
 		return -1;
 	}
 
+	// if target is in arr, return its index, otherwise return the index to be inserted
+	int bSearchInsert(int[] arr, int target) {
+		int start = 0, end = arr.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == target)
+				return mid;
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			}
+		}
+		return start;
+
+	}
+
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9 };
 		for (int i : arr)
 			System.out.print(new Solution().bSearch(arr, i) + ", ");
+		System.out.println();
+		int[] arr2 = { 1, 3, 5, 7, 9 };
+		for (int i = 0; i < 11; i++)
+			System.out
+					.println(i + ", " + new Solution().bSearchInsert(arr2, i));
 
 	}
 }
