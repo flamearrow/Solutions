baf6e95ad67e9d806517381a6f801d341a8450d1
flamearrow <arrow.cen@gmail.com>
2014-04-26 20:40:03 -0400

    new impl

--- src/longestSubstringWithoutRepeatingChars/Solution.java
+++ src/longestSubstringWithoutRepeatingChars/Solution.java
@@ -1,9 +1,38 @@
 package longestSubstringWithoutRepeatingChars;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //Given a string, find the length of the longest substring without repeating characters. 
 //For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
 //which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 public class Solution {
+	public int lengthOfLongestSubstring2(String s) {
+		int start = 0, end = 0;
+		Set<Character> visited = new HashSet<Character>();
+		int ret = 0;
+		while (end < s.length()) {
+			// find a dup, need to shrink window
+			if (visited.contains(s.charAt(end))) {
+				while (s.charAt(start) != s.charAt(end)) {
+					visited.remove(s.charAt(start));
+					start++;
+				}
+				// now s[start] == s[end], we need to furtherly increase start one step
+				start++;
+				end++;
+			}
+			// not visited, expand window
+			else {
+				visited.add(s.charAt(end));
+				end++;
+				if (end - start > ret)
+					ret = end - start;
+			}
+		}
+		return ret;
+	}
+
 	public int lengthOfLongestSubstring(String s) {
 		boolean[] seen = new boolean[26];
 		int start = 0, end = 0;
@@ -30,7 +59,8 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().lengthOfLongestSubstring(""));
+		System.out.println(new Solution()
+				.lengthOfLongestSubstring2("aabbccccde"));
 	}
 
 }
