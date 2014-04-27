58cabf4a5da787ad1fe8ba99fbed743188fd6840
flamearrow <arrow.cen@gmail.com>
2014-04-26 21:02:52 -0400

    re impl for add two numbers

--- src/addTwoNumbers/Solution.java
+++ src/addTwoNumbers/Solution.java
@@ -7,6 +7,28 @@ package addTwoNumbers;
 //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 //Output: 7 -> 0 -> 8
 public class Solution {
+	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		int carry = 0;
+		ListNode ret = null, cur = null, prev = null;
+		while (l1 != null || l2 != null) {
+			int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val)
+					+ carry;
+			cur = new ListNode(sum % 10);
+			carry = sum / 10;
+			if (ret == null) {
+				ret = cur;
+			} else {
+				prev.next = cur;
+			}
+			prev = cur;
+			l1 = l1 == null ? null : l1.next;
+			l2 = l2 == null ? null : l2.next;
+		}
+		if (carry > 0)
+			prev.next = new ListNode(1);
+		return ret;
+	}
+
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		ListNode retHead = null;
 		ListNode retPtr = null;
