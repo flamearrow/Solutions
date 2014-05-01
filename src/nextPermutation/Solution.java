f6c443a7e86a1bd7a11d3f58fec7e9518d3757f7
flamearrow <arrow.cen@gmail.com>
2014-04-30 22:17:43 -0400

    next permutation reimpl

--- src/nextPermutation/Solution.java
+++ src/nextPermutation/Solution.java
@@ -14,6 +14,48 @@ package nextPermutation;
 //3,2,1 -> 1,2,3
 //1,1,5 -> 1,5,1
 public class Solution {
+	public void nextPermutation2(int[] num) {
+		int cur = num.length - 1;
+		// if this loop overflow, then we would end up having number like 4321, which doesn't have the next permutation
+		while (cur > 0 && num[cur] <= num[cur - 1])
+			cur--;
+		if (cur == 0) {
+			// 4321 to 1234
+			reverse2(num, 0);
+			return;
+		}
+		// 1 5 9 6 4 2
+		//     |
+		//    cur
+		// num[cur] > num[cur-1]
+		int swapFrom = cur - 1;
+		int swapTo = cur;
+		while (swapTo + 1 < num.length && num[swapTo + 1] > num[swapFrom])
+			swapTo++;
+		//  from
+		//   |
+		// 1 5 9 6 4 2
+		//       |
+		//       to
+		int tmp = num[swapFrom];
+		num[swapFrom] = num[swapTo];
+		num[swapTo] = tmp;
+		// 1 6 9 5 4 2
+
+		reverse2(num, swapFrom + 1);
+	}
+
+	void reverse2(int[] num, int start) {
+		int end = num.length - 1;
+		while (end > start) {
+			int tmp = num[start];
+			num[start] = num[end];
+			num[end] = tmp;
+			start++;
+			end--;
+		}
+	}
+
 	// note we may have duplicate, need to assert for equal as well
 	public void nextPermutation(int[] num) {
 		int swapFrom = num.length - 2;
