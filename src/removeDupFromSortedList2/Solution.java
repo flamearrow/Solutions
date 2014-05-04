0e672277831b45dec67341db92fab257a092bd3a
flamearrow <arrow.cen@gmail.com>
2014-05-03 22:02:20 -0400

    remove dup form sorted list 2 reimpl

--- src/removeDupFromSortedList2/Solution.java
+++ src/removeDupFromSortedList2/Solution.java
@@ -7,6 +7,36 @@ package removeDupFromSortedList2;
 //Given 1->2->3->3->4->4->5, return 1->2->5.
 //Given 1->1->1->2->3, return 2->3. 
 public class Solution {
+	public ListNode deleteDuplicates2(ListNode head) {
+		if (head == null)
+			return head;
+		ListNode ret = null, cur = head, tail = null;
+		// make sure cur is always the first of its kind
+		while (cur != null) {
+			if (cur.next == null || cur.next.val != cur.val) {
+				if (ret == null) {
+					ret = cur;
+					tail = cur;
+				} else {
+					tail.next = cur;
+					tail = cur;
+				}
+				cur = cur.next;
+			}
+			// skip all dup numbers
+			else {
+				while (cur.next != null && cur.val == cur.next.val)
+					cur = cur.next;
+				// now cur.next is either null or a different node
+				cur = cur.next;
+			}
+		}
+		if (tail != null)
+			tail.next = null;
+
+		return ret;
+	}
+
 	public ListNode deleteDuplicates(ListNode head) {
 		// instead of removing dups,
 		// find distinct nodes that left and link them together
@@ -57,10 +87,10 @@ public class Solution {
 		ListNode head = new ListNode(1);
 		head.next = new ListNode(2);
 		head.next.next = new ListNode(2);
-//		head.next.next.next = new ListNode(2);
-//		head.next.next.next.next = new ListNode(3);
-//		head.next.next.next.next.next = new ListNode(4);
-//		head.next.next.next.next.next.next = new ListNode(5);
+		//		head.next.next.next = new ListNode(2);
+		//		head.next.next.next.next = new ListNode(3);
+		//		head.next.next.next.next.next = new ListNode(4);
+		//		head.next.next.next.next.next.next = new ListNode(5);
 		ListNode ret = new Solution().deleteDuplicates(head);
 		System.out.println(ret);
 	}
