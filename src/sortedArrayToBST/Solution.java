ed059d71df6057bbeedb19907c359ddfa98678ed
flamearrow <arrow.cen@gmail.com>
2014-05-05 21:26:18 -0400

    convert sorted array to BST reimpl

--- src/sortedArrayToBST/Solution.java
+++ src/sortedArrayToBST/Solution.java
@@ -2,6 +2,22 @@ package sortedArrayToBST;
 
 //Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 public class Solution {
+	public TreeNode sortedArrayToBST2(int[] num) {
+		return doConvert(num, 0, num.length - 1);
+	}
+
+	TreeNode doConvert(int[] num, int start, int end) {
+		if (start == end)
+			return new TreeNode(num[start]);
+		if (start > end)
+			return null;
+		int mid = (start + end) / 2;
+		TreeNode root = new TreeNode(num[mid]);
+		root.left = doConvert(num, start, mid - 1);
+		root.right = doConvert(num, mid + 1, end);
+		return root;
+	}
+
 	public TreeNode sortedArrayToBST(int[] num) {
 		return createTree(num, 0, num.length - 1);
 	}
