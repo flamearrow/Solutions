147aca9e2799ab8316deb0bbc788adb21ac1acfb
flamearrow <arrow.cen@gmail.com>
2014-05-11 18:45:01 -0400

    bits manipulation

---
+++ src/epi/bitToy/Solution.java
@@ -0,0 +1,15 @@
+package epi.bitToy;
+
+public class Solution {
+	// -i and ~(i-1) equates
+	static void doStuff() {
+		for (int i = 0; i < 100; i++) {
+			System.out.print((0 - i) + " - ");
+			System.out.println(~(i - 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		doStuff();
+	}
+}
