f4c09867ba27e3ca73dde2e18e6c3ccbd8c0adec
flamearrow <arrow.cen@gmail.com>
2014-05-12 23:26:19 -0400

    a toy implementation of h2o not sure if it's correct

---
+++ src/h20/Solution.java
@@ -0,0 +1,82 @@
+package h20;
+
+import java.util.concurrent.Semaphore;
+
+// implement h() and o() such that all initial calls will be block until
+//  there're two threads blocking on h() and one thread blocking o()
+//  in such case return these three threads
+public class Solution {
+	public static void main(String[] args) {
+		H2O h = new H2O();
+		new HCaller(h).start();
+		new OCaller(h).start();
+		new HCaller(h).start();
+		new HCaller(h).start();
+		new OCaller(h).start();
+		new HCaller(h).start();
+		new HCaller(h).start();
+		new OCaller(h).start();
+		new HCaller(h).start();
+		new OCaller(h).start();
+		new HCaller(h).start();
+	}
+}
+
+class HCaller extends Thread {
+	private H2O h;
+
+	public HCaller(H2O h) {
+		this.h = h;
+	}
+
+	@Override
+	public void run() {
+		h.h();
+	}
+}
+
+class OCaller extends Thread {
+	private H2O h;
+
+	public OCaller(H2O h) {
+		this.h = h;
+	}
+
+	@Override
+	public void run() {
+		h.o();
+	}
+}
+
+class H2O {
+	Semaphore sh, so;
+	int cnt;
+
+	public H2O() {
+		sh = new Semaphore(0);
+		so = new Semaphore(0);
+		cnt = 0;
+	}
+
+	public void h() {
+		so.release(1);
+		try {
+			sh.acquire(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("h() returns");
+	}
+
+	public void o() {
+		try {
+			so.acquire(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sh.release(2);
+		System.out.println("o() returns, " + ++cnt + " h20 is created");
+	}
+}
