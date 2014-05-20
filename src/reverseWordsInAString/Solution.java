416a22b7af5216d54e6da1cfa9eba21f891eca95
Arrow Cen <arrow.cen@gmail.com>
2014-05-20 11:33:13 -0400

    reverse words in a string reimpl

--- src/reverseWordsInAString/Solution.java
+++ src/reverseWordsInAString/Solution.java
@@ -16,6 +16,69 @@ package reverseWordsInAString;
 //   Reduce them to a single space in the reversed string.
 
 public class Solution {
+	// cheating
+	public String reverseWords2(String s) {
+		// again, in regex
+		//     +: one or more
+		//     *: none or more
+		//     ?: none or one
+		String[] ss = s.trim().split(" +");
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		for (String sss : ss) {
+			if (first) {
+				sb.insert(0, sss);
+				first = false;
+			} else {
+				sb.insert(0, ' ');
+				sb.insert(0, sss);
+			}
+		}
+		return sb.toString();
+	}
+
+	public String reverseWords4(String s) {
+		Stack<String> stack = new Stack<String>();
+		int cur = 0;
+		String next = null;
+		while (true) {
+			cur += skipSpace(s, cur);
+			if (cur >= s.length())
+				break;
+			next = getNextWord(s, cur);
+			stack.push(next);
+			cur += next.length();
+		}
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		while (!stack.isEmpty()) {
+			if (first) {
+				sb.append(stack.pop());
+				first = false;
+			} else {
+				sb.append(' ');
+				sb.append(stack.pop());
+			}
+		}
+		return sb.toString();
+	}
+
+	private int skipSpace(String s, int cur) {
+		int ret = 0;
+		while (cur < s.length() && s.charAt(cur) == ' ') {
+			cur++;
+			ret++;
+		}
+		return ret;
+	}
+
+	private String getNextWord(String s, int cur) {
+		int prob = cur;
+		while (prob < s.length() && s.charAt(prob) != ' ') {
+			prob++;
+		}
+		return s.substring(cur, prob);
+	}
 	public String reverseWords(String s) {
 		StringBuilder sb = new StringBuilder();
 		boolean first = true;
