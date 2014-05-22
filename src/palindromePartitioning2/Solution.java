ccc4ada362c50648a0ce3a059038738280683b34
flamearrow <arrow.cen@gmail.com>
2014-05-21 23:47:03 -0400

    palindrome partitioning reimpl: use a local 2D buffer

--- src/palindromePartitioning2/Solution.java
+++ src/palindromePartitioning2/Solution.java
@@ -1,5 +1,7 @@
 package palindromePartitioning2;
 
+import java.util.Arrays;
+
 //Given a string s, partition s such that every substring of the partition is a palindrome.
 //
 //Return the minimum cuts needed for a palindrome partitioning of s.
@@ -7,6 +9,98 @@ package palindromePartitioning2;
 //For example, given s = "aab",
 //Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut. 
 public class Solution {
+	// to make isPalindrome constant, we use a 2D array.
+	// each inner loop will check palindrome from shortest end
+	public int minCut3(String s) {
+		boolean[][] backSet = new boolean[s.length()][s.length()];
+		int[] ret = new int[s.length() + 1];
+		Arrays.fill(ret, Integer.MAX_VALUE);
+		ret[0] = -1;
+		for (int i = 1; i <= s.length(); i++) {
+			int minCut = Integer.MAX_VALUE;
+			// ret[j-1] is the minCut of s[0-j] inclusive
+			for (int j = i - 1; j >= 0; j--) {
+				// check if s[j to i-1] is palindrome, note (i-1-j) would grow from 0 to i-1 so that we always check shorter strings first
+				// i-j <= 2 corresponds to
+				//		1) j == i-1 : same char
+				//		2) j == i-1-1 : consecutive char
+				// if they're not , then just strip out head and tail of current string for subproblem checking, i.e j+1 and i-1-1
+				if ((s.charAt(j) == s.charAt(i - 1) && ((i - j <= 2) || backSet[j + 1][i - 2]))) {
+					backSet[j][i - 1] = true;
+				}
+				if (ret[j] < Integer.MAX_VALUE && backSet[j][i - 1]) {
+					minCut = Math.min(ret[j] + 1, minCut);
+				}
+			}
+			ret[i] = minCut;
+		}
+		return ret[s.length()];
+	}
+
+	public int minCut2(String s) {
+		boolean[][] backSet = new boolean[s.length()][s.length()];
+		int[] ret = new int[s.length() + 1];
+		Arrays.fill(ret, Integer.MAX_VALUE);
+		ret[0] = -1;
+		for (int i = 1; i <= s.length(); i++) {
+			int minCut = Integer.MAX_VALUE;
+			// ret[j-1] is the minCut of s[0-j] inclusive
+			for (int j = 0; j < i; j++) {
+				if (ret[j] < Integer.MAX_VALUE
+						&& isPalindrome2(s, j, i - 1, backSet)) {
+					minCut = Math.min(ret[j] + 1, minCut);
+				}
+			}
+			ret[i] = minCut;
+		}
+		return ret[s.length()];
+	}
+
+	boolean isPalindromeNaive(String s) {
+		int start = 0, end = s.length() - 1;
+		while (start < end) {
+			if (s.charAt(start) != s.charAt(end)) {
+				return false;
+			} else {
+				start++;
+				end--;
+			}
+		}
+		return true;
+	}
+
+	// check if s[start to end] inclusive is a palindrome
+	private boolean isPalindrome2(String s, int start, int end,
+			boolean[][] backSet) {
+		if (start == end) {
+			backSet[start][end] = true;
+			return true;
+		}
+
+		int bStart = start, bEnd = end;
+		while (start < end) {
+			if (backSet[start][end]) {
+				backSet[bStart][bEnd] = true;
+				return true;
+			}
+			if (s.charAt(start) == s.charAt(end)) {
+				start++;
+				end--;
+			} else {
+				backSet[bStart][bEnd] = false;
+				return false;
+			}
+		}
+
+		while (bStart < bEnd) {
+			backSet[bStart][bEnd] = true;
+			bStart++;
+			bEnd--;
+		}
+		return true;
+
+	}
+
 	boolean[][] backSet;
 
 	// using a O(n^2) dp approach,
@@ -72,6 +166,6 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().minCut("aabcde"));
+		System.out.println(new Solution().minCut3("aaab"));
 	}
 }
