88ba2c547ee2c81c079dc46bb599acb85580e14a
flamearrow <arrow.cen@gmail.com>
2014-05-24 15:59:13 -0400

    validParentheses reimpl

--- src/validParentheses/Solution.java
+++ src/validParentheses/Solution.java
@@ -1,6 +1,7 @@
 package validParentheses;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 //Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
 // determine if the input string is valid.
@@ -8,6 +9,34 @@ import java.util.LinkedList;
 //The brackets must close in the correct order, "()" and "()[]{}" are all 
 // valid but "(]" and "([)]" are not.
 public class Solution {
+
+	public boolean isValid2(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (left(c)) {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty())
+					return false;
+				else {
+					char left = stack.pop();
+					if (!matches(left, c))
+						return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	boolean left(char c) {
+		return c == '(' || c == '[' || c == '{';
+	}
+
+	boolean matches(char left, char right) {
+		return left == '(' && right == ')' || left == '[' && right == ']'
+				|| left == '{' && right == '}';
+	}
+
 	// (): parentheses
 	// []: brackets
 	// {}: braces or curly brackets
