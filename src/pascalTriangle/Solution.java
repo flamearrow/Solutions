5b9cc10e51b294b5a0aca68207877839b8934368
flamearrow <arrow.cen@gmail.com>
2014-06-26 23:15:24 -0400

    pascal triangle reimpl

--- src/pascalTriangle/Solution.java
+++ src/pascalTriangle/Solution.java
@@ -15,6 +15,24 @@ import java.util.ArrayList;
 // [1,4,6,4,1]
 //]
 public class Solution {
+	public static ArrayList<ArrayList<Integer>> generate2(int numRows) {
+		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> cur = new ArrayList<Integer>();
+		cur.add(1);
+		ret.add(new ArrayList<Integer>(cur));
+		for (int i = 1; i < numRows; i++) {
+			ArrayList<Integer> next = new ArrayList<Integer>();
+			next.add(1);
+			for (int j = 1; j < i; j++) {
+				next.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+			}
+			next.add(1);
+			ret.add(next);
+
+		}
+		return ret;
+	}
+
 	public ArrayList<ArrayList<Integer>> generate(int numRows) {
 		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
 		if (numRows == 0)
@@ -36,7 +54,7 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		for (ArrayList<Integer> lst : new Solution().generate(10)) {
+		for (ArrayList<Integer> lst : generate2(10)) {
 			for (int i : lst)
 				System.out.print(i + " ");
 			System.out.println();
