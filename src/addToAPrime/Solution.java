b166373e4a9c291a25f45d33877b84af01447aa4
Chen Cen <arrow.cen@gmail.com>
2014-06-27 17:10:50 -0400

    add to prime reimpl

--- src/addToAPrime/Solution.java
+++ src/addToAPrime/Solution.java
@@ -2,6 +2,40 @@ package addToAPrime;
 
 // Given a set of numbers [1-N], find the number of subsets such that sum of subset is a prime
 public class Solution {
+	
+	static int getNumSubsets2(int n) {
+		int max = (1 + n) * n / 2;
+		boolean[] primeMask = createPrimeMask(max);
+		// dp[i][j] is the number of subsets that added to j for the first i numbers
+		int[][] dp = new int[n + 1][max + 1];
+		// for first number 1, the only possible sum is 1, and there's only 1 subset
+		dp[1][1] = 1;
+		// to fill in the dp table:
+		//  1) a number i can add to it self: dp[i][i] += 1
+		//  2) not adding number i, get from the result of last row: dp[i][sum] += dp[i-1][sum]
+		//  3) adding number i, modify result from last row: dp[i][sum+i] += dp[i-1][sum]
+		for (int i = 2; i <= n; i++) {
+			// the number i can add to a sum i - we add a new combination
+			dp[i][i] += 1;
+			for (int sum = 1; sum <= max - i; sum++) {
+				// not adding i, the sum would be sum
+				dp[i][sum] += dp[i - 1][sum];
+				// adding i, the sum would be sum+i
+				dp[i][sum + i] += dp[i - 1][sum];
+			}
+		}
+
+		// now dp[n][1-max] would contain the number of subsets that first n numbers add to [1-max]
+		int ret = 0;
+		for (int k = 1; k <= max; k++) {
+			if (primeMask[k]) {
+				ret += dp[n][k];
+			}
+		}
+		return ret;
+	}
+	
+	
 	// dp[i][j] means the number of ways to add to number j in first i numbers
 	// after filling out dp table, we need to go through dp[n][0-maxSum] for all dp[n][mlgb] where mlgb is prime 
 	// we add the number of ways
