4df44f62262bf15e89fbc747af058e67cb1a1141
flamearrow <arrow.cen@gmail.com>
2014-06-29 13:54:42 -0400

    reconstruct tree in and pre order

--- src/constructBTreeFromInOrderAndPostOrder/Solution.java
+++ src/constructBTreeFromInOrderAndPostOrder/Solution.java
@@ -8,6 +8,36 @@ import java.util.Map;
 //Note:
 //You may assume that duplicates do not exist in the tree.
 public class Solution {
+	// the idea is to locate the range of post and in order array for each iteration
+	// for post order array, the sequence would be [leftSubTree][rightSubTree][root]
+	//  to recurse, we need to know the end of leftSubTree and rightSubTree
+	//  end of rightSubTree would be index[root]-1
+	//  end of leftSubTree would be index[root]-(sizeof(rightSubTree)-1)
+	//    where sizeof(rightSubTree) can be calculated from (inorder start and inorder root)
+	// similar idea applies to preorder traversal
+	public TreeNode buildTree3(int[] inorder, int[] postorder) {
+		if (inorder.length == 0)
+			return null;
+		Map<Integer, Integer> inIndexMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < inorder.length; i++) {
+			inIndexMap.put(inorder[i], i);
+		}
+		return doBuild3(inIndexMap, postorder, 0, inorder.length - 1,
+				inorder.length - 1);
+	}
+
+	TreeNode doBuild3(Map<Integer, Integer> inIndexMap, int[] postorder,
+			int inStart, int inEnd, int postEnd) {
+		if (inStart > inEnd)
+			return null;
+		TreeNode root = new TreeNode(postorder[postEnd]);
+		int inIndex = inIndexMap.get(postorder[postEnd]);
+		root.left = doBuild3(inIndexMap, postorder, inStart, inIndex - 1,
+				postEnd - inEnd + inIndex - 1);
+		root.right = doBuild3(inIndexMap, postorder, inIndex + 1, inEnd,
+				postEnd - 1);
+		return root;
+	}
 
 	public TreeNode buildTreeNew(int[] inorder, int[] postorder) {
 		if (inorder.length == 0)
@@ -71,9 +101,9 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		int[] inorder = {};
-		int[] postorder = {};
-		TreeNode root = new Solution().buildTree(inorder, postorder);
+		int[] inorder = { 2, 4, 1, 3, 6, 5 };
+		int[] postorder = { 4, 2, 6, 5, 3, 1 };
+		TreeNode root = new Solution().buildTree3(inorder, postorder);
 		System.out.println(root);
 	}
 
