76a4d72da9614c189d45587de535880ac338d282
flamearrow <arrow.cen@gmail.com>
2014-06-29 16:17:26 -0400

    same tree reimpl

--- src/sameTree/Solution.java
+++ src/sameTree/Solution.java
@@ -5,6 +5,15 @@ package sameTree;
 //Two binary trees are considered equal if they are structurally identical and 
 // the nodes have the same value. 
 public class Solution {
+	public boolean isSameTree2(TreeNode p, TreeNode q) {
+		if (p == null && q == null)
+			return true;
+		if (p == null || q == null)
+			return false;
+		return p.val == q.val && isSameTree(p.left, q.left)
+				&& isSameTree(p.right, q.right);
+	}
+
 	public boolean isSameTree(TreeNode p, TreeNode q) {
 		if (p == null && q == null)
 			return true;
