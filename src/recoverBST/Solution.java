02100411df070d64ba831cfed2c80b75f6d4ae4d
flamearrow <arrow.cen@gmail.com>
2014-06-29 16:42:02 -0400

    recover btree reimpl

--- src/recoverBST/Solution.java
+++ src/recoverBST/Solution.java
@@ -7,6 +7,50 @@ package recoverBST;
 //A solution using O(n) space is pretty straight forward. Could you devise
 // 	a constant space solution? 
 public class Solution {
+	TreeNode a, b, c, d, preNode;
+	int prev = Integer.MIN_VALUE;
+	
+	// the idea is to dfs the sequence and find swapped nodes
+	//  there'll be two scenarios:
+	//   1) we find two pairs: a<b, c<d
+	//			e.g 1 2 3 4 5 6
+	//		swapped 3 2 1 4 5 6
+	//		in this case we find 2<3 and 1<2, need to swap b and c, e.g 3 and 1
+	//   2) we only find one pair: a<b
+	//			e.g 1 2 3 4 5 6
+	//		swapped 2 1 3 4 5 6
+	//		in this case we find 1<2, need to swap a and b
+	public void recoverTree2(TreeNode root) {
+		probe(root);
+		if (c == null) {
+			int val = a.val;
+			a.val = b.val;
+			b.val = val;
+		} else {
+			int val = b.val;
+			b.val = c.val;
+			c.val = val;
+		}
+	}
+
+	void probe(TreeNode root) {
+		if (root == null)
+			return;
+		probe(root.left);
+		if (prev > root.val) {
+			if (a == null) {
+				b = preNode;
+				a = root;
+			} else {
+				d = preNode;
+				c = root;
+			}
+		}
+		preNode = root;
+		prev = root.val;
+		probe(root.right);
+	}
+
 	// we use potential Wrong node for the following tree
 	//    2
 	//  3   1
@@ -47,13 +91,13 @@ public class Solution {
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(4);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(6);
-		root.left.left = new TreeNode(3);
-		root.left.right = new TreeNode(1);
-		root.right.left = new TreeNode(5);
-		root.right.right = new TreeNode(7);
-		new Solution().recoverTree(root);
+		root.left = new TreeNode(6);
+		root.right = new TreeNode(2);
+		//		root.left.left = new TreeNode(3);
+		//		root.left.right = new TreeNode(1);
+		//		root.right.left = new TreeNode(5);
+		//		root.right.right = new TreeNode(7);
+		new Solution().recoverTree2(root);
 		System.out.println(root);
 	}
 }
