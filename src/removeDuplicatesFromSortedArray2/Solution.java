1cdfc89af6edfb71ead8df1d8a41af67aaceb4e0
flamearrow <arrow.cen@gmail.com>
2014-06-30 14:48:05 -0400

    remove dups reimpl

--- src/removeDuplicatesFromSortedArray2/Solution.java
+++ src/removeDuplicatesFromSortedArray2/Solution.java
@@ -8,6 +8,27 @@ package removeDuplicatesFromSortedArray2;
 //
 //Your function should return length = 5, and A is now [1,1,2,2,3]. 
 public class Solution {
+	public int removeDuplicates2(int[] A) {
+		if (A.length == 0)
+			return 0;
+		int dup = 1;
+		int cur = 1;
+		for (int i = 1; i < A.length; i++) {
+			if (A[i] == A[i - 1]) {
+				if (dup < 2) {
+					A[cur] = A[i];
+					cur++;
+				}
+				dup++;
+			} else {
+				dup = 1;
+				A[cur] = A[i];
+				cur++;
+			}
+		}
+		return cur;
+	}
+
 	public int removeDuplicates(int[] A) {
 		return removeDuplicatesAllowN(A, 2);
 	}
@@ -40,7 +61,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		int[] A = { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4 };
-		int newLen = new Solution().removeDuplicates(A);
+		int newLen = new Solution().removeDuplicates2(A);
 		System.out.println(newLen);
 		for (int i = 0; i < newLen; i++) {
 			System.out.print(A[i] + " ");
