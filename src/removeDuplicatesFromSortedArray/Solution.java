341166f5c4fa0271815bad354885d509ad701a9b
flamearrow <arrow.cen@gmail.com>
2014-07-04 17:40:34 -0400

    remove dup from sorted array reimpl

--- src/removeDuplicatesFromSortedArray/Solution.java
+++ src/removeDuplicatesFromSortedArray/Solution.java
@@ -11,6 +11,20 @@ package removeDuplicatesFromSortedArray;
 //
 //Your function should return length = 2, and A is now [1,2]. 
 public class Solution {
+
+	public int removeDuplicates2(int[] A) {
+		if (A.length == 0)
+			return 0;
+		int ret = 1, cur = 1;
+		while (cur < A.length) {
+			if (A[cur] != A[cur - 1]) {
+				A[ret++] = A[cur];
+			}
+			cur++;
+		}
+		return ret;
+	}
+
 	public int removeDuplicates(int[] A) {
 		if (A.length == 0)
 			return 0;
@@ -29,8 +43,8 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		int[] A = { 1, 1, 1, 1, 1, 1 };
-		int newLen = new Solution().removeDuplicates(A);
+		int[] A = {};
+		int newLen = new Solution().removeDuplicates2(A);
 		System.out.println(newLen);
 		for (int i = 0; i < newLen; i++) {
 			System.out.print(A[i] + " ");
