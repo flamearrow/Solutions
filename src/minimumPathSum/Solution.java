c2fff140abdb1b3faaa01c9c1537c4af016bf02a
flamearrow <arrow.cen@gmail.com>
2014-07-04 15:39:10 -0400

    pathSum reimpl

--- src/minimumPathSum/Solution.java
+++ src/minimumPathSum/Solution.java
@@ -4,6 +4,30 @@ package minimumPathSum;
 //
 //Note: You can only move either down or right at any point in time.
 public class Solution {
+	public static void main(String[] args) {
+		int[][] grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+		System.out.println(new Solution().minPathSum2(grid));
+	}
+
+	public int minPathSum2(int[][] grid) {
+		int width = grid[0].length;
+		int height = grid.length;
+		int[][] dp = new int[height][width];
+		dp[0][0] = grid[0][0];
+		for (int i = 1; i < width; i++) {
+			dp[0][i] = dp[0][i - 1] + grid[0][i];
+		}
+		for (int i = 1; i < height; i++) {
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
+		}
+		for (int i = 1; i < height; i++) {
+			for (int j = 1; j < width; j++) {
+				dp[i][j] = (dp[i - 1][j] < dp[i][j - 1] ? dp[i - 1][j]
+						: dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[height - 1][width - 1];
+	}
 
 	public int minPathSum(int[][] grid) {
 		int yLen = grid.length;
