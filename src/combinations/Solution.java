50130e37f07fd4e6d83d73b6fca3319f29e1e38f
flamearrow <arrow.cen@gmail.com>
2014-07-04 14:07:13 -0400

    combinations reimpl

--- src/combinations/Solution.java
+++ src/combinations/Solution.java
@@ -1,6 +1,7 @@
 package combinations;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 //Given two integers n and k, return all possible combinations 
 //of k numbers out of 1 ... n.
@@ -16,6 +17,32 @@ import java.util.ArrayList;
 //  [1,4],
 //]
 public class Solution {
+
+	public ArrayList<ArrayList<Integer>> combine2(int n, int k) {
+		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+		LinkedList<Integer> cur = new LinkedList<Integer>();
+		probe(ret, cur, 1, n, k);
+		return ret;
+	}
+
+	// return all eligible number combinations of size of count from start to end
+	// don't even care about the situation when there's not enough number
+	//  e.g from 4 to 5, find 6 numbers - use count<0 to terminate it
+	void probe(ArrayList<ArrayList<Integer>> ret, LinkedList<Integer> cur,
+			int start, int end, int count) {
+		if (count < 0)
+			return;
+		if (count == 0)
+			ret.add(new ArrayList<Integer>(cur));
+		else {
+			for (int i = start; i <= end; i++) {
+				cur.addLast(i);
+				probe(ret, cur, i + 1, end, count - 1);
+				cur.removeLast();
+			}
+		}
+	}
+
 	// recursion
 	public ArrayList<ArrayList<Integer>> combine(int n, int k) {
 		if (k == 0)
@@ -47,7 +74,7 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		ArrayList<ArrayList<Integer>> rst = new Solution().combine(0, 4);
+		ArrayList<ArrayList<Integer>> rst = new Solution().combine2(4, 4);
 		for (ArrayList<Integer> arr : rst) {
 			for (Integer i : arr) {
 				System.out.print(i + " ");
