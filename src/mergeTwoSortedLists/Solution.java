b5910866ccb5372441938deedad74bc48b876c4f
flamearrow <arrow.cen@gmail.com>
2014-07-04 16:52:01 -0400

    merge two sorted list reimpl

--- src/mergeTwoSortedLists/Solution.java
+++ src/mergeTwoSortedLists/Solution.java
@@ -3,6 +3,35 @@ package mergeTwoSortedLists;
 //Merge two sorted linked lists and return it as a new list. 
 //The new list should be made by splicing together the nodes of the first two lists.
 public class Solution {
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+		ListNode ret = null, pre = null;
+		while (l1 != null && l2 != null) {
+			ListNode next = l1.val < l2.val ? l1 : l2;
+
+			if (pre == null) {
+				ret = next;
+			} else {
+				pre.next = next;
+			}
+			pre = next;
+			if (next == l1)
+				l1 = l1.next;
+			if (next == l2)
+				l2 = l2.next;
+		}
+		ListNode left = l1 == null ? l2 : l1;
+		while (left != null) {
+			if (pre == null) {
+				ret = left;
+			} else {
+				pre.next = left;
+			}
+			pre = left;
+			left = left.next;
+		}
+		return ret;
+	}
+
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 		ListNode head = null, pre = null;
 		if (l1 != null && l2 != null) {
@@ -55,9 +84,16 @@ public class Solution {
 
 	public static void main(String[] args) {
 		ListNode left = new ListNode(2);
+		left.setNext(5).setNext(7);
 		ListNode right = new ListNode(3);
-		ListNode head = new Solution().mergeTwoLists(left, right);
+		right.setNext(6).setNext(8);
+		ListNode head = new Solution().mergeTwoLists2(left, null);
 		System.out.println(head);
+		ListNode cur = head;
+		while (cur != null) {
+			System.out.println(cur.val);
+			cur = cur.next;
+		}
 	}
 }
 
@@ -68,5 +104,10 @@ class ListNode {
 	ListNode(int x) {
 		val = x;
 		next = null;
+	}
+
+	public ListNode setNext(int next) {
+		this.next = new ListNode(next);
+		return this.next;
 	}
 }
