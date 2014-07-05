8cd3ea982d052e22723ea19508d1761d8c098a34
flamearrow <arrow.cen@gmail.com>
2014-07-05 12:28:01 -0400

    sudokusolver reimpl

--- src/sudokuSolver/Solution.java
+++ src/sudokuSolver/Solution.java
@@ -1,11 +1,141 @@
 package sudokuSolver;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 //Write a program to solve a Sudoku puzzle by filling the empty cells.
 //
 //Empty cells are indicated by the character '.'.
 //
 //You may assume that there will be only one unique solution. 
 public class Solution {
+
+	int nextX = -1, nextY = -1;
+
+	// don't use temporary node class
+	public void solveSudoku2(char[][] board) {
+		updateNext(board);
+		doProbe(board);
+	}
+
+	// from left to right, up to bottom, find the next place in board that's '.'
+	// if no such place, the board is populated, update (nextX, nextY) to (-1,-1)
+	void updateNext(char[][] board) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] == '.') {
+					nextX = i;
+					nextY = j;
+					return;
+				}
+			}
+		}
+		nextX = -1;
+		nextY = -1;
+	}
+
+	boolean doProbe(char[][] board) {
+		// full, done
+		if (nextX == -1)
+			return true;
+		// try all valid candidates and populate them to board[nextX][nextY]
+		List<Integer> candidates = getCandidates(board);
+		int curX = nextX, curY = nextY;
+		for (int i : candidates) {
+			board[curX][curY] = (char) ('0' + i);
+			updateNext(board);
+			if (doProbe(board)) {
+				return true;
+			} else {
+				// clear curX and curY so that updateNext() 
+				// will find the correct spot for next candidate
+				board[curX][curY] = '.';
+			}
+		}
+		// if we reach here then this board is not solvable
+		return false;
+	}
+
+	// return which numbers are valid at board[nextX][nextY]
+	List<Integer> getCandidates(char[][] board) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 1; i < 10; i++) {
+			set.add(i);
+		}
+		for (int i = 0; i < 9; i++) {
+			if (board[nextX][i] != '.') {
+				set.remove(board[nextX][i] - '0');
+			}
+			if (board[i][nextY] != '.') {
+				set.remove(board[i][nextY] - '0');
+			}
+		}
+
+		int startX = (nextX / 3) * 3;
+		int startY = (nextY / 3) * 3;
+		for (int i = startX; i < startX + 3; i++) {
+			for (int j = startY; j < startY + 3; j++) {
+				if (i != nextX && j != nextY && board[i][j] != '.') {
+					set.remove(board[i][j] - '0');
+				}
+			}
+		}
+		return new ArrayList<Integer>(set);
+	}
+
+	public static void main(String[] args) {
+		// int[][] b = { { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
+		// { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, { 1, 9, 8, 3, 4, 2, 5, 6, 7, },
+		// { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
+		// { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
+		// { 2, 8, 7, 4, 1, 9, 6, 3, 5 }, { 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
+
+		// int[][] b = { { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
+		// { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, { 1, 9, 8, 3, 4, 2, 5, 6, 7, },
+		// { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
+		// { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
+		// { 2, 8, 7, 4, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+		String[] bb = { "..9748...", "7........", ".2.1.9...", "..7...24.",
+				".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.." };
+		// char[][] board = getBoard(b);
+		char[][] board = getBoard(bb);
+		// char[][] board = new char[9][9];
+		// for (int i = 0; i < 9; i++) {
+		// for (int j = 0; j < 9; j++) {
+		// board[i][j] = '.';
+		// }
+		// }
+		new Solution().solveSudoku2(board);
+		System.out.println(board);
+
+	}
+
+	static char[][] getBoard(String[] board) {
+		char[][] ret = new char[9][9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				ret[i][j] = board[i].charAt(j);
+			}
+		}
+		return ret;
+	}
+
+	static char[][] getBoard(int[][] board) {
+		char[][] ret = new char[9][9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] != 0) {
+					ret[i][j] = (char) ('0' + board[i][j]);
+				} else {
+					ret[i][j] = '.';
+				}
+			}
+		}
+		return ret;
+	}
+
 	// naive DFS
 	public void solveSudoku(char[][] board) {
 		Node start = board[0][0] == '.' ? new Node(0, 0) : getNextNode(
@@ -77,57 +207,6 @@ public class Solution {
 		}
 		return null;
 	}
-
-	public static void main(String[] args) {
-		// int[][] b = { { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
-		// { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, { 1, 9, 8, 3, 4, 2, 5, 6, 7, },
-		// { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
-		// { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
-		// { 2, 8, 7, 4, 1, 9, 6, 3, 5 }, { 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
-
-		// int[][] b = { { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
-		// { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, { 1, 9, 8, 3, 4, 2, 5, 6, 7, },
-		// { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
-		// { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
-		// { 2, 8, 7, 4, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
-		String[] bb = { "..9748...", "7........", ".2.1.9...", "..7...24.",
-				".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.." };
-		// char[][] board = getBoard(b);
-		char[][] board = getBoard(bb);
-		// char[][] board = new char[9][9];
-		// for (int i = 0; i < 9; i++) {
-		// for (int j = 0; j < 9; j++) {
-		// board[i][j] = '.';
-		// }
-		// }
-		new Solution().solveSudoku(board);
-		System.out.println(board);
-
-	}
-
-	static char[][] getBoard(String[] board) {
-		char[][] ret = new char[9][9];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				ret[i][j] = board[i].charAt(j);
-			}
-		}
-		return ret;
-	}
-
-	static char[][] getBoard(int[][] board) {
-		char[][] ret = new char[9][9];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != 0) {
-					ret[i][j] = (char) ('0' + board[i][j]);
-				} else {
-					ret[i][j] = '.';
-				}
-			}
-		}
-		return ret;
-	}
 }
 
 class Node {
@@ -137,4 +216,5 @@ class Node {
 		this.x = x;
 		this.y = y;
 	}
+
 }
