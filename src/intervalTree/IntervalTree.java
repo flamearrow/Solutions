04d69e1f6d1e63b717b2b8c51219a1d11400d18a
flamearrow <arrow.cen@gmail.com>
2014-07-17 22:56:06 -0400

    interval tree

---
+++ src/intervalTree/IntervalTree.java
@@ -0,0 +1,102 @@
+package intervalTree;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IntervalTree {
+	INode root;
+
+	public IntervalTree(List<Interval> intervals) {
+		Iterator<Interval> itr = intervals.iterator();
+		if (!itr.hasNext())
+			return;
+
+		root = new INode(itr.next());
+		root.max = root.interval.end;
+
+		while (itr.hasNext()) {
+			insert(itr.next());
+		}
+	}
+
+	public static void main(String[] args) {
+		List<Interval> itvs = new LinkedList<Interval>();
+		itvs.add(new Interval(15, 20));
+		itvs.add(new Interval(10, 30));
+		itvs.add(new Interval(17, 19));
+		itvs.add(new Interval(5, 20));
+		itvs.add(new Interval(12, 15));
+		itvs.add(new Interval(30, 40));
+		IntervalTree tree = new IntervalTree(itvs);
+		System.out.println(tree);
+		System.out.println(tree.overLap(new Interval(6, 7)));
+	}
+
+	void insert(Interval interval) {
+		INode cur = root;
+		while (true) {
+			if (interval.end > cur.max) {
+				cur.max = interval.end;
+			}
+			if (interval.start < cur.interval.start) {
+				if (cur.left != null) {
+					cur = cur.left;
+				} else {
+					cur.left = new INode(interval);
+					cur.left.max = interval.end;
+					break;
+				}
+			} else {
+				if (cur.right != null) {
+					cur = cur.right;
+				} else {
+					cur.right = new INode(interval);
+					cur.right.max = interval.end;
+					break;
+				}
+			}
+		}
+	}
+
+	// return one interval
+	Interval overLap(Interval interval) {
+		INode cur = root;
+		// while there's a possible overlap 
+		while (cur.max > interval.start) {
+			if (cur.interval.start > interval.end) {
+				cur = cur.left;
+			} else if (cur.interval.end < interval.start) {
+				cur = cur.right;
+			} else {
+				return cur.interval;
+			}
+		}
+		return null;
+	}
+
+}
+
+class Interval {
+	int start, end;
+
+	public Interval(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	@Override
+	public String toString() {
+		return start + ":" + end;
+	}
+}
+
+class INode {
+	Interval interval;
+	int max;
+	INode left, right;
+
+	public INode(Interval interval) {
+		this.interval = interval;
+	}
+}
