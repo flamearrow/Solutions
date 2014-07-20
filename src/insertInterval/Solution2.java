604f0adaaaef4912c394cd8dac75917cc2b9b318
flamearrow <arrow.cen@gmail.com>
2014-07-19 21:54:28 -0400

    insert interval reimpl

--- src/insertInterval/Solution2.java
+++ src/insertInterval/Solution2.java
@@ -3,6 +3,49 @@ package insertInterval;
 import java.util.ArrayList;
 
 public class Solution2 {
+	public ArrayList<Interval> insert2(ArrayList<Interval> intervals,
+			Interval newInterval) {
+		ArrayList<Interval> ret = new ArrayList<Interval>();
+		Interval cur = newInterval;
+		boolean done = false;
+		for (Interval itv : intervals) {
+			if (done) {
+				ret.add(itv);
+			} else {
+				int rst = compareInterval(cur, itv);
+				// cur is before itv, add cur and next, copy over the remaining intervals
+				if (rst == -1) {
+					ret.add(cur);
+					ret.add(itv);
+					done = true;
+				}
+				// cur overlaps with itv, update cur
+				else if (rst == 0) {
+					cur.start = Math.min(cur.start, itv.start);
+					cur.end = Math.max(cur.end, itv.end);
+				}
+				// cur is after itv add itv
+				else if (rst == 1) {
+					ret.add(itv);
+				}
+			}
+		}
+		if (!done) {
+			ret.add(cur);
+		}
+		return ret;
+	}
+
+	int compareInterval(Interval i1, Interval i2) {
+		if (i1.end < i2.start) {
+			return -1;
+		} else if (i1.start > i2.end) {
+			return 1;
+		} else
+			return 0;
+
+	}
+
 	public ArrayList<Interval> insert(ArrayList<Interval> intervals,
 			Interval newInterval) {
 		ArrayList<Interval> ret = new ArrayList<Interval>();
